b5ff583ef0bf7bc535e4e1632e03f6ff00ac331f
Kira <kawoods826@gmail.com>
2020-01-24 11:45:56 -0800

    all test conditions working

--- src/evaluateGuesses.js
+++ src/evaluateGuesses.js
@@ -1,19 +1,40 @@
 import arrayEqual from "array-equal";
 import React from "react";
 
+let guessCount = 10;
+let codeLengthMinusOne = 3;
+
 export const evaluateGuesses = (code, guesses) => {
   let digitAndPositionCorrect = 0;
   let onlyDigitCorrect = 0;
+  let codeCopy = code;
+  let guessCopy = guesses[guesses.length - 1];
 
   if (arrayEqual(code, guesses[guesses.length - 1])) {
     return "You Won!";
-  } else if (guesses[guesses.length - 1] !== code && guesses.length >= 10) {
+  } else if (
+    guesses[guesses.length - 1] !== code &&
+    guesses.length >= guessCount
+  ) {
     return "You Lost :(";
   } else {
-    for (let i = 0; i <= 3; i++) {
+    for (let i = 0; i <= codeLengthMinusOne; i++) {
       if (code[i] === guesses[guesses.length - 1][i]) {
         digitAndPositionCorrect++;
+        codeCopy.splice(i, 1, null);
+        guessCopy.splice(i, 1, null);
       }
+    }
+    for (let i = 0; i <= 7; i++) {
+      let codeCopyCount = codeCopy.filter(function(number) {
+        return number === i;
+      }).length;
+
+      let guessCopyCount = guessCopy.filter(function(number) {
+        return number === i;
+      }).length;
+
+      onlyDigitCorrect += Math.min(codeCopyCount, guessCopyCount);
     }
     return (
       "Correct Digit and Position: " +
