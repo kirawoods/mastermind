3d5961880cfdc5e1f1361e96b2b0d7db2b496c24
Kira <kawoods826@gmail.com>
2020-01-23 22:58:16 -0800

    add condition to pass test with one digit correct in correct position and no digits correct and in incorrect position

--- src/evaluateGuesses.js
+++ src/evaluateGuesses.js
@@ -3,8 +3,27 @@ import arrayEqual from "array-equal";
 export const evaluateGuesses = (code, guesses) => {
   if (arrayEqual(code, guesses[guesses.length - 1])) {
     return "You Won!";
-  } else if (code !== guesses[guesses.length - 1] && guesses.length >= 10) {
+  } else if (guesses[guesses.length - 1] !== code && guesses.length >= 10) {
     return "You Lost :(";
+  } else if (
+    (code[0] === guesses[guesses.length - 1][0] &&
+      code[1] !== guesses[guesses.length - 1][1] &&
+      code[2] !== guesses[guesses.length - 1][2] &&
+      code[3] !== guesses[guesses.length - 1][3]) ||
+    (code[0] !== guesses[guesses.length - 1][0] &&
+      code[1] === guesses[guesses.length - 1][1] &&
+      code[2] !== guesses[guesses.length - 1][2] &&
+      code[3] !== guesses[guesses.length - 1][3]) ||
+    (code[0] !== guesses[guesses.length - 1][0] &&
+      code[1] !== guesses[guesses.length - 1][1] &&
+      code[2] === guesses[guesses.length - 1][2] &&
+      code[3] !== guesses[guesses.length - 1][3]) ||
+    (code[0] !== guesses[guesses.length - 1][0] &&
+      code[1] !== guesses[guesses.length - 1][1] &&
+      code[2] !== guesses[guesses.length - 1][2] &&
+      code[3] === guesses[guesses.length - 1][3])
+  ) {
+    return "0 correct digits in incorrect position, 1 correct digit in correct position";
   } else {
     return "Try Again";
   }
