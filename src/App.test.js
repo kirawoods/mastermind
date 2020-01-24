873deae4b365ad18884779efd4bf5ae5a88493a9
Kira <kawoods826@gmail.com>
2020-01-23 22:17:04 -0800

    create test for win condition

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import { evaluateGuess } from "./evaluateGuess";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("returns 'You Won!' when the last guess is equal to the code", () => {
+  const code = [1, 2, 3, 4];
+  const guess = [1, 2, 3, 4];
+  expect(evaluateGuess(code, guess)).toBe("You Won!");
 });
--- src/evaluateGuess.js
+++ src/evaluateGuess.js
@@ -1,7 +1,11 @@
-export const evaluateGuess = (code, guesses) => {
-  if (code === guesses[guesses.length - 1]) {
+import arrayEqual from "array-equal";
+
+export const evaluateGuess = (code, guess) => {
+  if (arrayEqual(code, guess)) {
     return "You Won!";
   } else if (code !== guesses[guesses.length - 1] && guesses.length >= 10) {
     return "You Lost :(";
+  } else {
+    return "Try Again, Loserino";
   }
 };
