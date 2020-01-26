39641875459d9382aa02282ad1f3b8863864a6b8
Kira <kawoods826@gmail.com>
2020-01-25 23:37:41 -0800

    added validation for input

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Game.css";
 import GuessHistory from "./GuessHistory";
+import { codeLength, allowedDigits } from "../evaluateGuesses";
 
 class Game extends Component {
   constructor(props) {
@@ -10,17 +11,33 @@ class Game extends Component {
     };
   }
 
+  allowedValues = (length, digits) => {
+    let value = 0;
+    for (let i = 0; i <= length - 1; i++) {
+      value += digits * Math.pow(10, i);
+    }
+    return value;
+  };
+
   handleClick = e => {
     e.preventDefault();
     const guess = document.getElementById("input").value;
-    const guessArray = Array.from(guess).map(el => parseInt(el));
-    console.log(guessArray);
-    this.setState({ guesses: [...this.state.guesses, guessArray] });
-    console.log(this.state);
-    let form = document.getElementById("input-form");
-    form.reset();
+    const allowedValue = this.allowedValues(codeLength, allowedDigits);
+    if (guess <= allowedValue) {
+      const guessArray = Array.from(guess).map(el => parseInt(el));
+      console.log(guessArray);
+      this.setState({
+        guesses: [...this.state.guesses, guessArray]
+      });
+      console.log(this.state);
+      let form = document.getElementById("input-form");
+      form.reset();
+    } else {
+      window.alert("Please enter a value between 0000 and " + allowedValue);
+    }
   };
   render() {
+    console.log(this.allowedValues(codeLength, allowedDigits));
     return (
       <div>
         <div className="GuessInput">
