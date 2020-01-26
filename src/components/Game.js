33a7a14d931df10dfec3a7a7401482b789af43bd
Kira <kawoods826@gmail.com>
2020-01-26 12:15:34 -0800

    make changes to game components for win loss pages

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
 import "./Game.css";
 import GuessHistory from "./GuessHistory";
-import { codeLength, allowedDigits } from "../evaluateGuesses";
+import arrayEqual from "array-equal";
+import axios from "axios";
+import uniqueID from "uniqid";
+import { WinPage } from "./WinPage";
+import { LosePage } from "./LosePage";
+import {
+  codeLength,
+  allowedDigits,
+  evaluateGuess,
+  guessesAllowed
+} from "../evaluateGuesses";
+
+let code = [];
+axios
+  .get(
+    `https://www.random.org/integers/?num=4&min=0&max=7&col=4&base=10&format=plain&rnd=new`
+  )
+  .then(response => {
+    const codeInfo = response.data;
+    code = codeInfo
+      .replace(/\s/g, "")
+      .split("")
+      .map(el => parseInt(el));
+    console.log(code);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 class Game extends Component {
   constructor(props) {
@@ -10,6 +37,15 @@ class Game extends Component {
       guesses: []
     };
   }
+
+  displayGuess = guess => {
+    return (
+      <div key={uniqueID()} className="each-guess">
+        <div className="guess">{guess.join("")}</div>
+        <div className="feedback">{evaluateGuess(code, guess)}</div>
+      </div>
+    );
+  };
 
   allowedValues = (length, digits) => {
     let value = 0;
@@ -42,27 +78,40 @@ class Game extends Component {
     }
   };
   render() {
-    return (
-      <div>
-        <div className="GuessInput">
-          <form id="input-form" autoComplete="off">
-            <input
-              id="input"
-              type="text"
-              className="digit-input"
-              required
-            ></input>
+    if (this.guesses.length !== 0) {
+      if (arrayEqual(code, this.guesses[this.guesses.length - 1])) {
+        return <WinPage />;
+      } else if (
+        this.guesses[this.guesses.length - 1] !== code &&
+        this.guesses.length >= guessesAllowed
+      ) {
+        return <LosePage />;
+      } else {
+        return (
+          <div>
+            <div className="GuessInput">
+              <form id="input-form" autoComplete="off">
+                <input
+                  id="input"
+                  type="text"
+                  className="digit-input"
+                  required
+                ></input>
 
-            <br></br>
-            <button onClick={this.handleClick} className="guess-button">
-              Make Guess
-            </button>
-          </form>
-        </div>
-        <GuessHistory guesses={this.state.guesses} />
-      </div>
-    );
+                <br></br>
+                <button onClick={this.handleClick} className="guess-button">
+                  Make Guess
+                </button>
+              </form>
+            </div>
+            <GuessHistory
+              guesses={this.state.guesses}
+              displayGuess={this.displayGuess}
+            />
+          </div>
+        );
+      }
+    }
   }
 }
-
 export default Game;
--- src/components/GuessHistory.js
+++ src/components/GuessHistory.js
@@ -1,84 +1,30 @@
 import React, { Component } from "react";
 import "./GuessHistory.css";
-import uniqueID from "uniqid";
-import axios from "axios";
+
 import arrayEqual from "array-equal";
 import { evaluateGuess, guessesAllowed } from "../evaluateGuesses";
 import { WinPage } from "./WinPage";
 import { LosePage } from "./LosePage";
-
-let code = [];
-axios
-  .get(
-    `https://www.random.org/integers/?num=4&min=0&max=7&col=4&base=10&format=plain&rnd=new`
-  )
-  .then(response => {
-    const codeInfo = response.data;
-    code = codeInfo
-      .replace(/\s/g, "")
-      .split("")
-      .map(el => parseInt(el));
-    console.log(code);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-const displayGuess = guess => {
-  return (
-    <div key={uniqueID()} className="each-guess">
-      <div className="guess">{guess.join("")}</div>
-      <div className="feedback">{evaluateGuess(code, guess)}</div>
-    </div>
-  );
-};
 
 class GuessHistory extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    console.log(this.props.guesses);
-    if (this.props.guesses.length !== 0) {
-      if (arrayEqual(code, this.props.guesses[this.props.guesses.length - 1])) {
-        return <WinPage />;
-      } else if (
-        this.props.guesses[this.props.guesses.length - 1] !== code &&
-        this.props.guesses.length >= guessesAllowed
-      ) {
-        return <LosePage />;
-      } else {
-        return (
-          <div className="GuessHistory">
-            <p className="guesses-remaining">
-              Guesses Remaining: {guessesAllowed - this.props.guesses.length}
-            </p>
-            <div className="guess-history-container">
-              <div className="guess-header">
-                <div className="guess">Guess</div>
-                <div className="feedback">Feedback</div>
-              </div>
-              {this.props.guesses.map(guess => displayGuess(guess))}
-            </div>
+    return (
+      <div className="GuessHistory">
+        <p className="guesses-remaining">
+          Guesses Remaining: {guessesAllowed - this.props.guesses.length}
+        </p>
+        <div className="guess-history-container">
+          <div className="guess-header">
+            <div className="guess">Guess</div>
+            <div className="feedback">Feedback</div>
           </div>
-        );
-      }
-    } else {
-      return (
-        <div className="GuessHistory">
-          <p className="guesses-remaining">
-            Guesses Remaining: {guessesAllowed - this.props.guesses.length}
-          </p>
-          <div className="guess-history-container">
-            <div className="guess-header">
-              <div className="guess">Guess</div>
-              <div className="feedback">Feedback</div>
-            </div>
-            {this.props.guesses.map(guess => displayGuess(guess))}
-          </div>
+          {this.props.guesses.map(guess => this.props.displayGuess(guess))}
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
