ee5e7840543eef9e66d366f5f13fc6c359d8604e
Kira <kawoods826@gmail.com>
2020-01-26 20:00:04 -0800

    update win and lose page to refresh on enter

--- src/components/LosePage.js
+++ src/components/LosePage.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./LosePage.css";
 
 export function LosePage() {
+  document.addEventListener("keypress", e => {
+    if (e.keyCode === 13) {
+      window.location.reload();
+    }
+  });
   return (
     <div className="LosePage">
       <h1 className="lose-header">You Lost :(</h1>
--- src/components/WinPage.js
+++ src/components/WinPage.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./WinPage.css";
 
 export function WinPage() {
+  document.addEventListener("keypress", e => {
+    if (e.keyCode === 13) {
+      window.location.reload();
+    }
+  });
   return (
     <div className="WinPage">
       <h1 className="win-header">You Win!</h1>
